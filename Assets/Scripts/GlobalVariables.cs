e94e00ee95e8da69950b2d4570ffb3a2e902df54
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-11-25 11:01:41 +0900

    First commit.

---
+++ Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariables : MonoBehaviour
+{
+    public static bool flagA;
+    public static bool flagB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerPrefManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPrefManager : MonoBehaviour
+{
+    public GameObject TextA;
+    public GameObject TextB;
+
+    bool flagA;
+    bool flagB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int prefValue = PlayerPrefs.GetInt("FlagA");
+        if ( prefValue == 0)
+        {
+            flagA = false;
+        }else if ( prefValue == 1)
+        {
+            flagA = true;
+        }
+        GlobalVariables.flagA = flagA;
+
+        prefValue = PlayerPrefs.GetInt("FlagB");
+        if (prefValue == 0)
+        {
+            flagB = false;
+        }
+        else if (prefValue == 1)
+        {
+            flagB = true;
+        }
+        GlobalVariables.flagB = flagB;
+
+        setFlagToText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if ( GlobalVariables.flagA == true)
+            {
+                GlobalVariables.flagA = false;
+                PlayerPrefs.SetInt("FlagA", 0);
+            }
+            else
+            {
+                GlobalVariables.flagA = true;
+                PlayerPrefs.SetInt("FlagA", 1);
+            }
+
+            setFlagToText();
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (GlobalVariables.flagB == true)
+            {
+                GlobalVariables.flagB = false;
+                PlayerPrefs.SetInt("FlagB", 0);
+            }
+            else
+            {
+                GlobalVariables.flagB = true;
+                PlayerPrefs.SetInt("FlagB", 1);
+            }
+            setFlagToText();
+        }
+    }
+
+    void setFlagToText()
+    {
+        if ( GlobalVariables.flagA)
+        {
+            TextA.GetComponent<Text>().text = "On";
+        }
+        else
+        {
+            TextA.GetComponent<Text>().text = "Off";
+        }
+
+        if (GlobalVariables.flagB)
+        {
+            TextB.GetComponent<Text>().text = "On";
+        }
+        else
+        {
+            TextB.GetComponent<Text>().text = "Off";
+        }
+    }
+}
